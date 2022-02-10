0072be5b47352302c464ecd62106a490cdba3f49
Leandro <leandro.v.pordeus@gmail.com>
2022-02-10 16:29:36 -0300

    PROJETO COMITADO NO FINAL SOMENTE

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Hotelzinho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hospede[] vet = new Hospede[10];
+
+            Console.Write("Quantos Quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Aluguel #{i}:");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                vet[quarto] = new Hospede(nome, email);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quarto Ocupado: ");
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (vet[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vet[i]);
+                }
+            }
+        }
+    }
+}
